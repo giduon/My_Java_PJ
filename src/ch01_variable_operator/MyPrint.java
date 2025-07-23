aa6d0a5c96f719f2a32af2c9fd69a935b0ba034c
ict <giduon@naver.com>
2025-07-23 14:26:48 +0900

    자바 프로그래밍 실습

--- src/ch01_variable_operator/MyPrint.java
+++ src/ch01_variable_operator/MyPrint.java
@@ -1,4 +1,22 @@
 package ch01_variable_operator;
 
 public class MyPrint {
+    public static void main(String[] args) {
+
+        System.out.println("홍");
+        System.out.println();
+        System.out.println("길");
+        System.out.println("동");
+
+        System.out.print("대한 민국");
+        System.out.print(" 화이팅");
+        System.out.println();
+        System.out.println("\"예체능\" 프로그램 메인 MC '강호동'");
+
+        System.out.println();
+        System.out.println("이름\t국어\t영어\t수학");
+        System.out.println("김철수\t50\t60\t70");
+        System.out.println("박영희\t30\t40\t50");
+
+    }
 }
